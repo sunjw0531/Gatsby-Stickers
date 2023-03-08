9f780a864c3fc75791047fd6d5303355ac808e16
정승우 <sunjw0531@gmail.com>
2023-03-08 14:14:14 +0900

    Data Fetching
    It's the normal way when we use React.
    The difference with Gatsby and React is that, the user can see all thing in a HTML or wait for a data after user enter the website.
    So if we want Gatsby way, then we use the GraphQL

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
 
 export default function Blog() {
+  const [blogPosts, setBlogPosts] = useState();
+  useEffect(() => {
+    fetchBlogPosts();
+  }, []);
   return (
     <Layout title="Blog">
       <p>The most recent news from my shop.</p>
+      {blogPosts}
     </Layout>
   );
 }
