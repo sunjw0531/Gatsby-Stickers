3f3b02b9fe4353308c08f895f3e56298afa79fe0
정승우 <sunjw0531@gmail.com>
2023-03-28 21:38:16 +0900

    Dynamic Pages

---
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout';
+import React from 'react';
+export default function BlogPost() {
+  return (
+    <Layout title="Blog Post">
+      <div />
+    </Layout>
+  );
+}
